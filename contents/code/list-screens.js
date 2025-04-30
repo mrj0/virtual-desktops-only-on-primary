e14ef814ef9c4df88e6f0e98d299a2cb516e6276
Mike Johnson <mike@publicstatic.net>
2025-04-30 14:29:39 -0700

    Use screen names instead of index
    (indexes may change when monitors power off)

--- contents/code/list-screens.js
+++ contents/code/list-screens.js
@@ -1,4 +1,6 @@
-for (var i = 0; i < workspace.screens.length; i++) {
-    var screen = workspace.screens[i];
-    print("Screen #" + i + ": Name: " + screen.name + ", Brand: " + screen.manufacturer + ", Model: " + screen.model);
+for (let i = 0; i < workspace.screens.length; i++) {
+  const screen = workspace.screens[i];
+  console.log(
+    `Screen #${i}: Name: ${screen.name}, Brand: ${screen.manufacturer}, Model: ${screen.model}`,
+  );
 }
